f30058fafa1f70227a3f374b7705a2851bc7b916
Juliano Schroeder <juliano7s@gmail.com>
2012-01-09 19:57:17 -0200

    missing Entity.h and added to gitignore

---
+++ Goliath/Entity.h
@@ -0,0 +1,40 @@
+#ifndef _ENTITIES_H
+#define _ENTITIES_H
+
+#include "Number2D.h"
+
+class Entity
+{
+    public:
+        Entity(Number2D, Number2D, bool);
+        ~Entity(void);
+
+        Number2D getDimensions2D();
+        int getWidth();
+        int getHeight();
+
+        Number2D getPosition2D();
+        int getX();
+        int getY();
+
+        Number2D getSpeed2D();
+        int getSpeedX();
+        int getSpeedY();
+
+        bool isPassable();
+
+        void setSpeed(Number2D);
+        void setPostion(Number2D);
+
+    private:
+        int mWidth, mHeight;
+        int mPositionX, mPositionY;
+        int mSpeedX, mSpeedY;
+        bool mPassable; // for collision detection
+
+        //Number2D attributes
+        Number2D mDimension2D;
+        Number2D mPosition2D;
+        Number2D mSpeed2D;
+};
+#endif //_ENTITIES_H
