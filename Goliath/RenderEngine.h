c26c62390933151993f62e2b1752d7644090c7a1
Martin balmaceda <martin.balmaceda@gmail.com>
2012-01-11 21:28:40 +0200

    start of world rendering

--- Goliath/RenderEngine.h
+++ Goliath/RenderEngine.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "Entity.h"
+#include "World.h"
 
 class RenderEngine
 {
@@ -14,6 +15,7 @@ class RenderEngine
 		void InitCamera();
         void RenderWorld(Entity *being);
         void RenderEntities();
+        void RenderWorld2(World *world); // FIXME
         
     private:
         int mWindowWidth, mWindowHeight;
