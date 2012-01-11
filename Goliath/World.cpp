314870bff8d95db7528acb512b7d86748a1b887d
Martin balmaceda <martin.balmaceda@gmail.com>
2012-01-11 18:16:44 +0200

    cleanup

--- Goliath/World.cpp
+++ Goliath/World.cpp
@@ -4,6 +4,18 @@ World::World(int width, int height)
 {
     mWidth = width;
 	mHeight = height;
+	
+	std::vector< std::vector<Tile> > matrix;
+	
+	/*int **matrix = new bool*[width];
+	
+	for (int i = 0; i < width; ++i)
+	{
+		for (int j = 0; j < height; ++j)
+		{
+			matrix[i][j] = false;
+		}	
+	}*/
 }
 
 World::~World()
@@ -19,4 +31,9 @@ int World::getWidth()
 int World::getHeight()
 {
 	return mHeight;
+}
+
+bool World::isTilePassable(int x, int y)
+{
+	return false;
 }
